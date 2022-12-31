d621e2670f57e531d4a686644aafce8cf2ef78d3
Glen Newton <glen.newton@gmail.com>
2022-12-31 06:35:24 -0500

    All combos described; chunked mmap

--- pkg/bigarray/alt/alt.go
+++ pkg/bigarray/alt/alt.go
@@ -4,28 +4,38 @@ type Mode int8
 
 const (
 	RandomAccess Mode = iota
-	OrderedAccess
+	SeqentialAccess
 )
 
-type Sized int8
+type NItems int8
 
 const (
-	KnownSize Sized = iota
-	UnknownSize
+	Known Sized = iota
+	Unknown
 )
 
 /*
+        Write            / Read             NItems**        : Reader       / Writer
+        -------------------------------------------------------------------------------
+	SequentialAccess / SequentialAccess Unknown   : linear       / mmap|*linear
+       	SequentialAccess / SequentialAccess Known     : mmap|*linear / mmap|*linear
+        SequentialAccess / RandomAccess     Unknown   : linear       / mmap
+       	SequentialAccess / RandomAccess     Known     : mmap|*linear / mmap
 
-	OrderedAccess/OrderedAccess UnknownSize: linear/mmap|*linear
-       	OrderedAccess/OrderedAccess KnownSize  : mmap|*linear/mmap|*linear
-        OrderedAccess/RandomAccess UnknownSize: linear/mmap
-       	OrderedAccess/RandomAccess KnownSize  : mmap|*linear/mmap
+       	RandomAccess     / SequentialAccess Known     : mmap         / mmap|*linear
+       	RandomAccess     / SequentialAccess Unknown   : kv           / kv
+       	RandomAccess     / RandomAccess     Known     : mmap         / mmap
+       	RandomAccess     / RandomAccess     Unknown   : kv           / kv
 
-       	RandomAccess/OrderedAccess KnownSize   : mmap/mmap|linear
-       	RandomAccess/OrderedAccess UnknownSize : kv/linear
-       	RandomAccess/RandomAccess KnownSize   : mmap/mmap
-       	RandomAccess/RandomAccess UnknownSize : kv/mmap
+    * - Preferred implementation
+   ** - Number of total items to be written, at the time of start of write
 
+   Need: Writer: linear, mmap, kv
+         Reader: linear, mmap, kv
+
+   Idea: mmap chunker; array of mmaps, each a certain size (10,000 x SizeOf)
+         Get(i int64): which mmap: a[i/10000]; where in this mmap? i%10000
+         (for unknown array size)
 */
 
 type Serializer[T any] interface {
@@ -38,27 +48,23 @@ type Deserializer[T any] interface {
 	SizeOf() int
 }
 
-type Writer[T any] interface {
-	SetSerializer(Serializer[T]) error
-	GetSerializer() Serializer[T]
+type Writer interface {
 	Write(int64, []byte) error
 }
 
-type Reader[T any] interface {
-	SetDeserializer(Deserializer[T]) error
-	GetDeserializer() Deserializer[T]
+type Reader interface {
 	Read(int64) ([]byte, error)
 }
 
-func Put[T any](w Writer[T], index int64, value *T) error {
-	buf, err := w.GetSerializer().Serialize(*value)
+func Put[T any](w Writer, s Serializer[T], index int64, value *T) error {
+	buf, err := s.Serialize(*value)
 	if err != nil {
 		return err
 	}
 	return w.Write(index, buf)
 }
 
-func Get[T any](w Reader[T], index int64) (*T, error) {
+func Get[T any](w Reader, d Deserializer[T], index int64) (*T, error) {
 	return nil, nil
 }
 
